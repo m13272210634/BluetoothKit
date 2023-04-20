76faefe08740f41795b36d8cbe84f0b44d9650df
wangchunjie <wangchunjie@cs-novel.com>
2023-04-20 11:15:17 +0800

    113

--- Source/BKPeripheralConfiguration.swift
+++ Source/BKPeripheralConfiguration.swift
@@ -37,7 +37,7 @@ public class BKPeripheralConfiguration: BKConfiguration {
 
     // MARK: Initialization
 
-    public init(dataServiceUUID: UUID, dataServiceCharacteristicUUID: UUID, localName: String? = nil) {
+    public init(dataServiceUUID: CBUUID, dataServiceCharacteristicUUID: CBUUID, localName: String? = nil) {
         self.localName = localName
         super.init(dataServiceUUID: dataServiceUUID, dataServiceCharacteristicUUID: dataServiceCharacteristicUUID)
     }
